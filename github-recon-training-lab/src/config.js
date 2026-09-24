b8ac25952d1ed8f8dee0a3c17fd14d941df92e27
mahfuj <104072939+arifenmahfuj@users.noreply.github.com>
2026-09-24 18:30:15 +0600

    Update API and internal service URLs in config

--- github-recon-training-lab/src/config.js
+++ github-recon-training-lab/src/config.js
@@ -1,7 +1,7 @@
 const config = {
-  apiBaseUrl: "https://api.training-example.invalid",
-  internalService: "http://internal-service.training.invalid",
-  demoToken: "TRAINING_TOKEN_DO_NOT_USE"
+  apiBaseUrl: "https://api.mahfujwhh.net",
+  internalService: "http://internal-service.mahfujwhh.net",
+  Token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6ImFkbWluIFVzZXIiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTE2MjM5MDIyfQ.dGhpcy1pcy1hLWZha2Utc2lnbmF0dXJlLWZvci1kZW1vLXB1cnBvc2VzLW9ubHk"
 };
 
 module.exports = config;
